a5ac0a0e2460b98c57b63ea79e65512a610cd9a6
adoyaio <james@Jamess-MacBook-Pro.local>
2020-01-05 06:00:07 -0800

    support for cpi history table in local environment

--- blackBox/Data/dynamo/setup.lcl.py
+++ blackBox/Data/dynamo/setup.lcl.py
@@ -1,14 +1,18 @@
 from __future__ import print_function # Python 2/3 compatibility
+
+import decimal
+import json
 import boto3
 
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
 
+# CREATE DB TABLES
 table = dynamodb.create_table(
     TableName='branch_commerce_events',
     KeySchema=[
         {
             'AttributeName': 'branch_commerce_event_key',
-            'KeyType': 'HASH'  #Partition key defined by concatenating last_attributed_touch_data_tilde_campaign_id and last_attributed_touch_data_tilde_keyword
+            'KeyType': 'HASH'  #Partition key defined by concatenating campaign_id, ad_set_id and keyword
         },
         {
             'AttributeName': 'timestamp',
@@ -31,15 +35,14 @@ table = dynamodb.create_table(
         'WriteCapacityUnits': 10
     }
 )
-print("Table status:", table.table_status)
-
+print("Table status:", table.table_name, table.table_status)
 
 table = dynamodb.create_table(
     TableName='branch_opens',
     KeySchema=[
         {
             'AttributeName': 'branch_open_key',
-            'KeyType': 'HASH'  #Partition key defined by concatenating last_attributed_touch_data_tilde_campaign_id and last_attributed_touch_data_tilde_keyword
+            'KeyType': 'HASH'  #Partition key defined by concatenating campaign_id, ad_set_id and keyword
         },
         {
             'AttributeName': 'timestamp',
@@ -62,14 +65,14 @@ table = dynamodb.create_table(
         'WriteCapacityUnits': 10
     }
 )
-print("Table status:", table.table_status)
+print("Table status:", table.table_name, table.table_status)
 
 table = dynamodb.create_table(
     TableName='branch_installs',
     KeySchema=[
         {
             'AttributeName': 'branch_install_key',
-            'KeyType': 'HASH'  #Partition key defined by concatenating last_attributed_touch_data_tilde_campaign_id and last_attributed_touch_data_tilde_keyword
+            'KeyType': 'HASH' #Partition key defined by concatenating campaign_id, ad_set_id and keyword
         },
         {
             'AttributeName': 'timestamp',
@@ -92,15 +95,14 @@ table = dynamodb.create_table(
         'WriteCapacityUnits': 10
     }
 )
-print("Table status:", table.table_status)
-
+print("Table status:", table.table_name, table.table_status)
 
 table = dynamodb.create_table(
     TableName='branch_reinstalls',
     KeySchema=[
         {
             'AttributeName': 'branch_reinstall_key',
-            'KeyType': 'HASH'  #Partition key defined by concatenating last_attributed_touch_data_tilde_campaign_id and last_attributed_touch_data_tilde_keyword
+            'KeyType': 'HASH'  #Partition key defined by concatenating campaign_id, ad_set_id and keyword
         },
         {
             'AttributeName': 'timestamp',
@@ -123,4 +125,55 @@ table = dynamodb.create_table(
         'WriteCapacityUnits': 10
     }
 )
-print("Table status:", table.table_status)
+print("Table status:", table.table_name, table.table_status)
+
+table = dynamodb.create_table(
+    TableName='cpi_history',
+    KeySchema=[
+        {
+            'AttributeName': 'org_id',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'timestamp',
+            'KeyType': 'RANGE'  #Sort key
+        },
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'org_id',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'timestamp',
+            'AttributeType': 'S'
+        }
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 10,
+        'WriteCapacityUnits': 10
+    }
+)
+print("Table status:", table.table_name, table.table_status)
+
+# LOAD SAMPLE DATA
+with open("history_sample_data.lcl.json") as json_file:
+    cpi_lines = json.load(json_file, parse_float=decimal.Decimal)
+    for cpi_line in cpi_lines:
+        timestamp = cpi_line['date']
+        spend = cpi_line['spend']
+        installs = int(cpi_line['installs'])
+        cpi = cpi_line['cpi']
+        org_id = cpi_line['org_id']
+
+        print("Adding cpi line:", org_id, timestamp)
+
+        table.put_item(
+           Item={
+               'timestamp': timestamp,
+               'spend': spend,
+               'installs': installs,
+               'cpi' : cpi,
+               'org_id' : org_id
+            }
+        )
