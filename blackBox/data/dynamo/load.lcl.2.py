44282069a305033640418f0f8ece9d879dab661a
james <james@adoya.io>
2020-05-31 17:26:08 -0700

    sync local db with prod

--- blackBox/data/dynamo/load.lcl.2.py
+++ blackBox/data/dynamo/load.lcl.2.py
@@ -12,6 +12,7 @@ dynamodbProd = boto3.resource('dynamodb', region_name='us-east-1')
 clientsTableLocal = dynamodbLocal.Table('clients')
 clientsTableProd = dynamodbProd.Table('clients')
 items = clientsTableProd.scan()["Items"]
+
 for item in items:
     try:
         response = clientsTableLocal.put_item(
@@ -19,8 +20,8 @@ for item in items:
         )
     except ClientError as e:
         print("clients failed due to" + e.response['Error']['Message'])
-    else:
-        print("clients PutItem succeeded:")
+
+print("client rows added:::" + str(len(items)))
 
 # cpi_history table
 clientsTableLocal = dynamodbLocal.Table('cpi_history')
@@ -33,11 +34,35 @@ for item in items:
         )
     except ClientError as e:
         print("cpi_history failed due to" + e.response['Error']['Message'])
-    else:
-        print("cpi_history PutItem succeeded:")
 
+print("cpi_history rows added:::" + str(len(items)))
 
+# cpi_history table
+clientsTableLocal = dynamodbLocal.Table('apple_keyword')
+clientsTableProd = dynamodbProd.Table('apple_keyword')
+items = clientsTableProd.scan()["Items"]
+for item in items:
+    try:
+        response = clientsTableLocal.put_item(
+            Item=item
+        )
+    except ClientError as e:
+        print("apple_keyword failed due to" + e.response['Error']['Message'])
 
+print("apple_keyword rows added:::" + str(len(items)))
+
+clientsTableLocal = dynamodbLocal.Table('branch_commerce_events')
+clientsTableProd = dynamodbProd.Table('branch_commerce_events')
+items = clientsTableProd.scan()["Items"]
+for item in items:
+    try:
+        response = clientsTableLocal.put_item(
+            Item=item
+        )
+    except ClientError as e:
+        print("branch_commerce_events failed due to" + e.response['Error']['Message'])
+
+print("branch_commerce_events rows added:::" + str(len(items)))
 
  # LOAD SAMPLE DATA json
 # with open("../clients.json") as json_file:
