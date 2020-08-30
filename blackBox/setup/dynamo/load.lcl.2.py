3f4f3e2708a2cda067ff1b7a5a9deaf866387002
James <jarfarri@gmail.com>
2020-08-29 17:19:25 -0700

    page through keywords

--- blackBox/setup/dynamo/load.lcl.2.py
+++ blackBox/setup/dynamo/load.lcl.2.py
@@ -8,83 +8,98 @@ from botocore.exceptions import ClientError
 dynamodbLocal = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://localhost:8000")
 dynamodbProd = boto3.resource('dynamodb', region_name='us-east-1')
 
-# client table
-tableName = 'clients'
-local = dynamodbLocal.Table(tableName)
-prod = dynamodbProd.Table(tableName)
-items = prod.scan()["Items"]
 
-for item in items:
-    try:
-        response = local.put_item(
-            Item=item
-        )
-    except ClientError as e:
-        print(tableName + " failed due to" + e.response['Error']['Message'])
-
-print(tableName + " rows added:::" + str(len(items)))
-
-# cpi_history table
-tableName = 'cpi_history'
-local = dynamodbLocal.Table(tableName)
-prod = dynamodbProd.Table(tableName)
-items = prod.scan()["Items"]
-
-for item in items:
-    try:
-        response = local.put_item(
-            Item=item
-        )
-    except ClientError as e:
-        print(tableName + " failed due to" + e.response['Error']['Message'])
-
-print(tableName + " rows added:::" + str(len(items)))
-
-# apple_keyword table
-tableName = 'apple_keyword'
-local = dynamodbLocal.Table(tableName)
-prod = dynamodbProd.Table(tableName)
-items = prod.scan()["Items"]
-
-for item in items:
-    try:
-        response = local.put_item(
-            Item=item
-        )
-    except ClientError as e:
-        print(tableName + " failed due to" + e.response['Error']['Message'])
-
-print(tableName + " rows added:::" + str(len(items)))
+def load_items_to_local(items, local, tableName):
+    for item in items:
+        try:
+            response = local.put_item(
+                Item=item
+            )
+        except ClientError as e:
+            print(tableName + " failed due to" + e.response['Error']['Message'])
+    print(tableName + " rows added:::" + str(len(items)))
 
 
-# branch_commerce_events
-tableName = 'branch_commerce_events'
-local = dynamodbLocal.Table(tableName)
-prod = dynamodbProd.Table(tableName)
-items = prod.scan()["Items"]
+if __name__ == '__main__':
+    # client table
+    tableName = 'clients'
+    local = dynamodbLocal.Table(tableName)
+    prod = dynamodbProd.Table(tableName)
+    items = prod.scan()["Items"]
 
-for item in items:
-    try:
-        response = local.put_item(
-            Item=item
-        )
-    except ClientError as e:
-        print(tableName + " failed due to" + e.response['Error']['Message'])
+    for item in items:
+        try:
+            response = local.put_item(
+                Item=item
+            )
+        except ClientError as e:
+            print(tableName + " failed due to" + e.response['Error']['Message'])
 
-print(tableName + " rows added:::" + str(len(items)))
+    print(tableName + " rows added:::" + str(len(items)))
 
-# cpi_branch_history table
-tableName = 'cpi_branch_history'
-local = dynamodbLocal.Table(tableName)
-prod = dynamodbProd.Table(tableName)
-items = prod.scan()["Items"]
+    # cpi_history table
+    tableName = 'cpi_history'
+    local = dynamodbLocal.Table(tableName)
+    prod = dynamodbProd.Table(tableName)
+    items = prod.scan()["Items"]
 
-for item in items:
-    try:
-        response = local.put_item(
-            Item=item
-        )
-    except ClientError as e:
-        print(tableName + " failed due to" + e.response['Error']['Message'])
+    for item in items:
+        try:
+            response = local.put_item(
+                Item=item
+            )
+        except ClientError as e:
+            print(tableName + " failed due to" + e.response['Error']['Message'])
 
-print(tableName + " rows added:::" + str(len(items)))
+    print(tableName + " rows added:::" + str(len(items)))
+
+    # apple_keyword table
+    tableName = 'apple_keyword'
+    local = dynamodbLocal.Table(tableName)
+    prod = dynamodbProd.Table(tableName)
+    # items = prod.scan()["Items"]
+
+    done = False
+    start_key = None
+    scan_kwargs = {}
+
+    while not done:
+        if start_key:
+            scan_kwargs['ExclusiveStartKey'] = start_key
+        response = prod.scan(**scan_kwargs)
+        load_items_to_local(response.get('Items', []), local, tableName)
+        start_key = response.get('LastEvaluatedKey', None)
+        done = start_key is None
+
+
+    # branch_commerce_events
+    tableName = 'branch_commerce_events'
+    local = dynamodbLocal.Table(tableName)
+    prod = dynamodbProd.Table(tableName)
+    items = prod.scan()["Items"]
+
+    for item in items:
+        try:
+            response = local.put_item(
+                Item=item
+            )
+        except ClientError as e:
+            print(tableName + " failed due to" + e.response['Error']['Message'])
+
+    print(tableName + " rows added:::" + str(len(items)))
+
+    # cpi_branch_history table
+    tableName = 'cpi_branch_history'
+    local = dynamodbLocal.Table(tableName)
+    prod = dynamodbProd.Table(tableName)
+    items = prod.scan()["Items"]
+
+    for item in items:
+        try:
+            response = local.put_item(
+                Item=item
+            )
+        except ClientError as e:
+            print(tableName + " failed due to" + e.response['Error']['Message'])
+
+    print(tableName + " rows added:::" + str(len(items)))
