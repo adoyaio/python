05c59e45ab23b89a6e127890d27c9eec0d3469d9
James <jarfarri@gmail.com>
2021-12-05 13:31:08 -0800

    campaign reporting

--- blackBox/clientApi.py
+++ blackBox/clientApi.py
@@ -313,18 +313,36 @@ def getClientCostHistoryHandler(event, context):
     org_id = queryStringParameters["org_id"]
     dynamodb = LambdaUtils.getApiEnvironmentDetails(event).get('dynamodb')
 
-    query_by_time = False
-    try:
-        total_recs = queryStringParameters["total_recs"]
-    except KeyError as error:
-        query_by_time = True
+    offset = {
+        "org_id": queryStringParameters.get("offsetOrgId"),
+        "date": queryStringParameters.get("offsetDate"),
+    }
 
-    if query_by_time:
-        start_date = queryStringParameters["start_date"]
-        end_date = queryStringParameters["end_date"]
-        history = DynamoUtils.getClientBranchHistoryByTime(dynamodb, org_id, start_date, end_date)
-    else:
-        history = DynamoUtils.getClientBranchHistory(dynamodb, org_id, total_recs)
+    total_recs = queryStringParameters.get("total_recs", "100")
+    start_date = queryStringParameters.get("start_date", "all")
+    end_date = queryStringParameters.get("end_date", "all")
+
+    response = DynamoUtils.getClientBranchHistoryByTime(
+        dynamodb,
+        org_id,
+        total_recs,
+        offset,
+        start_date,
+        end_date
+    )
+
+    # query_by_time = False
+    # try:
+    #     total_recs = queryStringParameters["total_recs"]
+    # except KeyError as error:
+    #     query_by_time = True
+
+    # if query_by_time:
+    #     start_date = queryStringParameters["start_date"]
+    #     end_date = queryStringParameters["end_date"]
+    #     history = DynamoUtils.getClientBranchHistoryByTime(dynamodb, org_id, start_date, end_date)
+    # else:
+    #     history = DynamoUtils.getClientBranchHistory(dynamodb, org_id, total_recs)
 
     return {
         'statusCode': 200,
@@ -333,7 +351,7 @@ def getClientCostHistoryHandler(event, context):
             'Access-Control-Allow-Methods': 'GET',
             'Access-Control-Allow-Headers': 'x-api-key'
         },
-        'body': json.dumps(history, cls=DecimalEncoder)
+        'body': json.dumps(response, cls=DecimalEncoder)
     }
 
 
